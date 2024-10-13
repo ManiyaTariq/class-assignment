94d87a78c2e322705a45043aedfd808d2e9c35e5
ManiyaTariq <162306425+ManiyaTariq@users.noreply.github.com>
2024-10-13 23:55:59 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,13 @@
+ import Hero from "./components/hero/page";
+import Navbar from "./components/Navbar/page";
+import image from "next/image";
+
+
+
+export default function Home(){
+  return <div>
+    <Navbar/>
+   
+    <Hero/>
+  </div>
+}
